6259641ed3ed19a840097a20f7728da27aa3eaaa
DutchJavaDev <borismulder@outlook.com>
2025-05-20 18:31:20 +0200

    endpoints for gin

---
+++ MyMusicApi/http/handlers.go
@@ -0,0 +1,96 @@
+package http
+
+import (
+	"api/db"
+	"api/logging"
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lrstanley/go-ytdlp"
+)
+
+func AddMusic(ctx *gin.Context) {
+	var musicData db.Music
+
+	_ = ctx.ShouldBindBodyWithJSON(&musicData)
+
+	db.InsertMusic(&musicData)
+
+	ctx.JSON(200, gin.H{"message": "done"})
+}
+
+func GetMusic(ctx *gin.Context) {
+	music := db.GetMusic()
+	ctx.JSON(200, music)
+}
+
+// Downloads and converts playlist videos to audio only
+func DownloadPlaylist(ctx *gin.Context) {
+	var urlRequest UrlRequest
+
+	ctx.ShouldBindBodyWithJSON(&urlRequest)
+
+	go downloadPlaylist(urlRequest.Url)
+
+	ctx.String(200, "Started downloading playlist...")
+}
+
+// Exports playlist data to file
+// Does not download and convert video
+func DryRun(ctx *gin.Context) {
+	var urlRequest UrlRequest
+
+	ctx.ShouldBindBodyWithJSON(&urlRequest)
+
+	go dryRun(urlRequest.Url)
+
+	ctx.String(200, "Doing a dry run!")
+}
+
+func dryRun(link string) {
+	dl := ytdlp.New().
+		SkipDownload().
+		ForceIPv4().
+		SleepInterval(5).
+		MaxSleepInterval(20).
+		PrintToFile("[%(playlist)s] %(webpage_url)s %(title)s", "playlist_info").
+		Cookies("selenium/cookies_netscape")
+
+	result, err := dl.Run(context.TODO(), link)
+
+	if err != nil {
+		logging.Log(err.Error())
+	}
+
+	logging.Log(result)
+}
+
+func downloadPlaylist(playlistUrl string) {
+	dl := ytdlp.New().
+		FormatSort("bestaudio").
+		ExtractAudio().
+		AudioFormat("opus").
+		PostProcessorArgs("FFmpegExtractAudio:-b:a 160k").
+		DownloadArchive("video_archive.db").
+		ForceIPv4().
+		ParseMetadata("").
+		NoKeepVideo().
+		Output("music/%(playlist)s/%(title)s.%(ext)s").
+		SleepInterval(5).MaxSleepInterval(15).
+		Cookies("selenium/cookies_netscape")
+
+	result, errr := dl.Run(context.TODO(), playlistUrl)
+
+	if errr != nil {
+		logging.Log("#yt-dlp Failed")
+		logging.Log(errr.Error())
+		return
+	}
+
+	logging.Log("#stdout")
+	logging.Log(result.Stdout)
+
+	for _, log := range result.OutputLogs {
+		logging.Log(log.Line)
+	}
+}
