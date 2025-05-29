5e1109e1764a367aa66c7dac71d9b7d3467dcc80
DutchJavaDev <Borismulder@outlook.com>
2025-05-29 17:48:33 +0000

    background logging

--- MyMusicApi/logging/logger.go
+++ MyMusicApi/logging/logger.go
@@ -1,17 +1,21 @@
 package logging
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var OutputLog bool
 
 func Info(a any) {
-	log(fmt.Sprintf("\033[32m[API Info]\033[0m %s", a))
+	go log(fmt.Sprintf("\033[32m[API Info]\033[0m %s", a))
 }
-func Error(a any) {
-	log(fmt.Sprintf("\033[31m[API Error]\033[0m %s", a))
-}
+
 func Warning(a any) {
-	log(fmt.Sprintf("\033[33m[API Warning]\033[0m %s", a))
+	go log(fmt.Sprintf("\033[33m[API Warning]\033[0m %s", a))
+}
+
+func Error(a any) {
+	go log(fmt.Sprintf("\033[31m[API Error]\033[0m %s", a))
 }
 
 func log(text string) {
