4aa37ecd0c13fe229d34ce1b850f1b6c0a031fa0
DutchJavaDev <Borismulder@outlook.com>
2025-05-29 13:24:58 +0000

    log with colors to console

--- MyMusicApi/logging/logger.go
+++ MyMusicApi/logging/logger.go
@@ -4,8 +4,18 @@ import "fmt"
 
 var OutputLog bool
 
-func Log(a any) {
+func Info(a any) {
+	log(fmt.Sprintf("\033[32m[API Info]\033[0m %s", a))
+}
+func Error(a any) {
+	log(fmt.Sprintf("\033[31m[API Error]\033[0m %s", a))
+}
+func Warning(a any) {
+	log(fmt.Sprintf("\033[33m[API Warning]\033[0m %s", a))
+}
+
+func log(text string) {
 	if OutputLog {
-		fmt.Println(a)
+		fmt.Println(text)
 	}
 }
