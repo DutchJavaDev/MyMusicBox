e92c2371d526583e1975e4fb8e17b2a446fa166b
DutchJavaDev <Borismulder@outlook.com>
2025-06-02 20:56:57 +0000

    task enum

--- MyMusicApi/models/database.go
+++ MyMusicApi/models/database.go
@@ -2,12 +2,22 @@ package models
 
 import "time"
 
+type TaskState int
+
+const (
+	Pending TaskState = iota
+	Downloading
+	Updating
+	Done
+	Error
+)
+
 type Song struct {
 	Id        int       `json:"id" db:"id"`
 	Name      string    `json:"name" db:"name"`
 	SourceURL string    `json:"source_url" db:"sourceurl"`
 	Path      *string   `json:"path,omitempty" db:"path"`
-	Duration  *int      `json:"duration,omitempty" db:"duration"`
+	Duration  int       `json:"duration,omitempty" db:"duration"`
 	CreatedAt time.Time `json:"created_at" db:"createdat"`
 	UpdatedAt time.Time `json:"updated_at" db:"updatedat"`
 }
