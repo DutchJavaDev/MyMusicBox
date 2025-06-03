f7adc4fa51ba74ce3d911f332459ab28811d9d23
DutchJavaDev <Borismulder@outlook.com>
2025-06-03 20:44:06 +0000

    goal of this branch

--- MyMusicApi/util/ytdlp.go
+++ MyMusicApi/util/ytdlp.go
@@ -4,30 +4,42 @@ import (
 	"api/db"
 	"api/logging"
 	"api/models"
-	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/lrstanley/go-ytdlp"
 )
 
-func StartDownloadTask(taskId int, downloadRequest models.DownloadRequest) {
+func StartDownloadTask(taskId int, downloadRequest models.DownloadRequestModel) {
 	db := db.PostgresDb{}
 
 	if db.OpenConnection() {
 
 		defer db.CloseConnection()
 
-		storageFolderName := "music_dev"
+		storageFolderName := Config.SourceFolder
 		archiveFileName := fmt.Sprintf("%s/video_archive", storageFolderName)
 		idsFileName := fmt.Sprintf("%s/ids.%d", storageFolderName, taskId)
 		namesFileName := fmt.Sprintf("%s/names.%d", storageFolderName, taskId)
 		durationFileName := fmt.Sprintf("%s/durations.%d", storageFolderName, taskId)
-		urlsFileName := fmt.Sprintf("%s/urls.%d", storageFolderName, taskId)
-		fileExtension := "opus"
+		playlistTitleFileName := fmt.Sprintf("%s/playlist_title.%d", storageFolderName, taskId)
+		playlistIdFileName := fmt.Sprintf("%s/playlist_id.%d", storageFolderName, taskId)
+		fileExtension := Config.OutputExtension
+
+		cleanupFile := []string{
+			idsFileName,
+			namesFileName,
+			durationFileName,
+			playlistTitleFileName,
+			playlistIdFileName,
+		}
+
+		isPlaylist := strings.Contains(downloadRequest.Url, "playlist?")
 
 		dlp := ytdlp.New().
 			FormatSort("bestaudio").
@@ -41,17 +53,15 @@ func StartDownloadTask(taskId int, downloadRequest models.DownloadRequest) {
 			PrintToFile("%(id)s", idsFileName).
 			PrintToFile("%(title)s", namesFileName).
 			PrintToFile("%(duration)s", durationFileName).
-			PrintToFile("%(webpage_url)s", urlsFileName).
 			Output(storageFolderName + "/%(id)s.%(ext)s").
 			SleepInterval(5).
 			MaxSleepInterval(20).
 			Cookies("selenium/cookies_netscape")
 
-			/*
-				n_entries (numeric): Total number of extracted items in the playlist
-				playlist_id (string): Identifier of the playlist that contains the video'
-				for playlist the image extension is jpg
-			*/
+		if isPlaylist {
+			dlp = dlp.PrintToFile("%(playlist_title)s", playlistTitleFileName)
+			dlp = dlp.PrintToFile("%(playlist_id)s", playlistIdFileName)
+		}
 
 		// Update task status
 		db.UpdateTaskLogStatus(taskId, int(models.Downloading))
@@ -63,15 +73,19 @@ func StartDownloadTask(taskId int, downloadRequest models.DownloadRequest) {
 			query := `UPDATE TaskLog
 		             SET Status = $1, OutputLog = $2, EndTime = $3
 		             WHERE Id = $4`
-			// Store output
-			// Set Task state -> Error
-			err := db.NonScalarQuery(query, int(models.Error), result.Stderr, time.Now(), taskId)
 
+			// Set Task state -> Error
+			json, err := json.Marshal(result.OutputLogs)
+
+			err = db.NonScalarQuery(query, int(models.Error), json, time.Now(), taskId)
 			if err != nil {
 				logging.Error(fmt.Sprintf("Failed to update tasklog: %s", err.Error()))
 			}
 
-			// Delete created files?
+			// Delete created files if any
+			for _, path := range cleanupFile {
+				os.Remove(path)
+			}
 			return
 		}
 
@@ -82,52 +96,77 @@ func StartDownloadTask(taskId int, downloadRequest models.DownloadRequest) {
 		ids, _ := ReadLines(idsFileName)
 		names, _ := ReadLines(namesFileName)
 		durations, _ := ReadLines(durationFileName)
-		urls, _ := ReadLines(urlsFileName)
+
+		playlistId := -1
+		if isPlaylist {
+			// create new playlist
+			name, _ := ReadLines(playlistTitleFileName)
+
+			// Check if exists, if not then create
+			existingPlaylists, _ := db.FetchPlaylists(context.Background())
+
+			playlistExists := false
+
+			for _, playlist := range existingPlaylists {
+				if playlist.Name == name[0] {
+					playlistExists = true
+					playlistId = playlist.Id
+					break
+				}
+			}
+
+			if !playlistExists {
+				desc := "Custom playlist"
+				_playlistId, _ := ReadLines(playlistIdFileName)
+				playlistId, err = db.InsertPlaylist(models.Playlist{
+					Name:          name[0],
+					Description:   &desc,
+					ThumbnailPath: fmt.Sprintf("%s.jpg", _playlistId[0]),
+					CreationDate:  time.Now(),
+					IsPublic:      true,
+					UpdatedAt:     time.Now(),
+				})
+			}
+		}
 
 		var song models.Song
 
 		for id := range len(ids) {
 			song.Name = names[id]
 			song.Duration, _ = strconv.Atoi(durations[id])
-			song.SourceURL = urls[id]
-
-			path := fmt.Sprintf("%s/%s.%s", storageFolderName, ids[id], fileExtension)
-			song.Path = &path
-
+			song.SourceId = ids[id]
+			song.Path = fmt.Sprintf("%s/%s.%s", storageFolderName, ids[id], fileExtension)
+			song.ThumbnailPath = fmt.Sprintf("%s.webp", ids[id])
 			id, err := db.InsertSong(song)
 
 			if err != nil {
 				logging.Error(fmt.Sprintf("[StartDownloadTask] Failed to insert song (%d): %s", id, err.Error()))
 			}
+
+			if isPlaylist {
+				// add to playlist
+				db.InsertPlaylistSong(playlistId, id)
+			}
 		}
 
 		// Set task state -> Done
 		// Update task status
-		db.UpdateTaskLogStatus(taskId, int(models.Done))
+		query := `UPDATE TaskLog
+		             SET Status = $1, OutputLog = $2, EndTime = $3
+		             WHERE Id = $4`
+
+		json, err := json.Marshal(result.OutputLogs)
+
+		err = db.NonScalarQuery(query, int(models.Done), json, time.Now(), taskId)
+		if err != nil {
+			logging.Error(fmt.Sprintf("Failed to update tasklog: %s", err.Error()))
+		}
 
 		// Delete created files
-		os.Remove(idsFileName)
-		os.Remove(namesFileName)
-		os.Remove(durationFileName)
-		os.Remove(urlsFileName)
+		for _, path := range cleanupFile {
+			os.Remove(path)
+		}
 	} else {
 		logging.Error(fmt.Sprintf("[StartDownloadTask] Failed to open database connection: %s", db.Error.Error()))
 	}
-}
-
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func ReadLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
 }
