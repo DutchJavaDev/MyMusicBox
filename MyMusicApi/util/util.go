0988cf5ef3bd2e01e154f414d784cb97da72aeb5
DutchJavaDev <Borismulder@outlook.com>
2025-06-03 20:42:18 +0000

    update env/config values

--- MyMusicApi/util/util.go
+++ MyMusicApi/util/util.go
@@ -2,22 +2,19 @@ package util
 
 import (
 	"api/models"
+	"bufio"
 	"flag"
+	"os"
 )
 
 var Config models.Config
 
 func LoadConfig() {
-	var devPort string
-	flag.StringVar(&devPort, "port", "", "development port")
-
-	var devUrl bool
-	flag.BoolVar(&devUrl, "devurl", false, "rewrite  modify urls for dev")
-
+	flag.StringVar(&Config.DevPort, "port", "", "Development port else use default port")
+	flag.BoolVar(&Config.UseDevUrl, "devurl", false, "Have a dev prefix in the url")
+	flag.StringVar(&Config.SourceFolder, "sourceFolder", "music", "Output folder for data")
+	flag.StringVar(&Config.OutputExtension, "outputExtension", "opus", "Extension for ouput file")
 	flag.Parse()
-
-	Config.DevPort = devPort
-	Config.UseDevUrl = devUrl
 }
 
 func GetApiGroupUrlV1(useDevUrl bool) string {
@@ -26,4 +23,21 @@ func GetApiGroupUrlV1(useDevUrl bool) string {
 	} else {
 		return "/api/v1"
 	}
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
