0438fb22b82ff1115e45bce18a5c28a389ccdd8e
DutchJavaDev <Borismulder@outlook.com>
2025-06-03 20:43:22 +0000

    model fixed

--- MyMusicApi/models/database.go
+++ MyMusicApi/models/database.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type TaskState int
 
@@ -12,28 +15,38 @@ const (
 	Error
 )
 
+type TaskLog struct {
+	Id        int             `json:"id" db:"id"`
+	StartTime time.Time       `json:"startTime" db:"starttime"`
+	EndTime   *time.Time      `json:"endTime,omitempty" db:"endtime"`     // Nullable
+	Status    int             `json:"status" db:"status"`                 // Expected to be 0–4
+	OutputLog json.RawMessage `json:"outputLog,omitempty" db:"outputlog"` // JSONB field
+}
+
 type Song struct {
-	Id        int       `json:"id" db:"id"`
-	Name      string    `json:"name" db:"name"`
-	SourceURL string    `json:"source_url" db:"sourceurl"`
-	Path      *string   `json:"path,omitempty" db:"path"`
-	Duration  int       `json:"duration,omitempty" db:"duration"`
-	CreatedAt time.Time `json:"created_at" db:"createdat"`
-	UpdatedAt time.Time `json:"updated_at" db:"updatedat"`
+	Id            int       `json:"id" db:"id"`
+	Name          string    `json:"name" db:"name"`
+	SourceId      string    `json:"source_id" db:"sourceid"`
+	ThumbnailPath string    `json:"thumbnail_path" db:"thumbnailpath"`
+	Path          string    `json:"path,omitempty" db:"path"`
+	Duration      int       `json:"duration,omitempty" db:"duration"`
+	CreatedAt     time.Time `json:"created_at" db:"createdat"`
+	UpdatedAt     time.Time `json:"updated_at" db:"updatedat"`
 }
 
 type Playlist struct {
-	Id           int       `json:"id"`
-	Name         string    `json:"name"`
-	Description  *string   `json:"description,omitempty"` // nullable
-	CreationDate time.Time `json:"creationDate"`
-	UpdatedAt    time.Time `json:"updatedAt"`
-	IsPublic     bool      `json:"isPublic"`
+	Id            int       `json:"id" db:"id"`
+	Name          string    `json:"name" db:"name"`
+	Description   *string   `json:"description,omitempty" db:"description"`
+	ThumbnailPath string    `json:"thumbnailPath" db:"thumbnailpath"`
+	CreationDate  time.Time `json:"creationDate" db:"creationdate"`
+	UpdatedAt     time.Time `json:"updatedAt" db:"updatedat"`
+	IsPublic      bool      `json:"isPublic" db:"ispublic"`
 }
 
 type PlaylistSong struct {
-	SongID     int       `json:"songId"`
-	PlaylistID int       `json:"playlistId"`
-	Position   int       `json:"position"`
-	AddedAt    time.Time `json:"addedAt"`
+	SongId     int       `json:"songId" db:"songid"`
+	PlaylistId int       `json:"playlistId" db:"playlistid"`
+	Position   *int      `json:"position,omitempty" db:"position"` // Nullable
+	AddedAt    time.Time `json:"addedAt" db:"addedat"`
 }
