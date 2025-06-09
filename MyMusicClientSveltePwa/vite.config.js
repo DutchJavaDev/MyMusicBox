ca071dcadb85cc6292e18de0fd0f1275c0158e50
DutchJavaDev <borismulder@outlook.com>
2025-06-09 17:11:33 +0200

    initial commit

---
+++ MyMusicClientSveltePwa/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'MyMusic Client Svelte PWA',
+        short_name: 'MyMusic Svelte PWA',
+        description: 'A Progressive Web App for MyMusic Client built with Svelte',
+        start_url: '/',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
