62b08c3403c2021b568d2d6acdf6a9c484ff6a53
DutchJavaDev <Borismulder@outlook.com>
2025-06-09 12:08:41 +0000

    A bit better

--- MyMusicBoxApi/database/tasklog.go
+++ MyMusicBoxApi/database/tasklog.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"musicboxapi/logging"
 	"musicboxapi/models"
+	"time"
 )
 
 func (pdb *PostgresDb) InsertTaskLog() (lastInsertedId int, error error) {
@@ -17,37 +19,40 @@ func (pdb *PostgresDb) InsertTaskLog() (lastInsertedId int, error error) {
 func (pdb *PostgresDb) UpdateTaskLogStatus(taskId int, nStatus int) (error error) {
 	query := `UPDATE TaskLog SET Status = $1 WHERE Id = $2`
 
-	// could add request context?
-	transaction, err := pdb.connection.Begin()
-
-	statement, err := transaction.Prepare(query)
-	defer statement.Close()
-
-	if err != nil {
-		transaction.Rollback()
-		logging.Error(fmt.Sprintf("[UpdateTaskLogStatus] Prepared statement error: %s", err.Error()))
-		return err
-	}
-
-	_, err = statement.Exec(nStatus, taskId)
-
-	if err != nil {
-		logging.Error(fmt.Sprintf("[UpdateTaskLogStatus] Queryrow error: %s", err.Error()))
-		transaction.Rollback()
-		return err
-	}
-
-	err = transaction.Commit()
-
-	if err != nil {
-		logging.Error(fmt.Sprintf("[UpdateTaskLogStatus] Transaction commit error: %s", err.Error()))
-		transaction.Rollback()
-		return err
-	}
-
-	return nil
+	return pdb.NonScalarQuery(query, nStatus, taskId)
 }
 
-// TODO
-// func (pdb *PostgresDb) AddLog(log models.Log) {
-// }
+func (pdb *PostgresDb) EndTaskLog(taskId int, nStatus int, data []byte) error {
+	query := `UPDATE TaskLog SET Status = $1, OutputLog = $2, EndTime = $3 WHERE Id = $4`
+
+	return pdb.NonScalarQuery(query, nStatus, data, time.Now(), taskId)
+}
+
+func (pdb *PostgresDb) GetTaskLogs(ctx context.Context) ([]models.TaskLog, error) {
+	query := `SELECT Id, StartTime, EndTime, Status, OutputLog FROM TaskLog ORDER BY Id desc` // get the latest first
+
+	rows, err := pdb.connection.QueryContext(ctx, query)
+	defer rows.Close()
+
+	if err != nil {
+		logging.Error(fmt.Sprintf("[GetTaskLogs] QueryRow error: %s", err.Error()))
+		return nil, err
+	}
+
+	var tasklog models.TaskLog
+
+	tasks := make([]models.TaskLog, 0)
+
+	for rows.Next() {
+		scanError := rows.Scan(&tasklog.Id, &tasklog.StartTime, &tasklog.EndTime, &tasklog.Status, &tasklog.OutputLog)
+
+		if scanError != nil {
+			logging.Error(fmt.Sprintf("[GetTaskLogs] Scan error: %s", scanError.Error()))
+			continue
+		}
+
+		tasks = append(tasks, tasklog)
+	}
+
+	return tasks, nil
+}
