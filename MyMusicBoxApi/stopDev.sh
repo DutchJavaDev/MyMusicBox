da308c7472ce68ddc04d53f8c9f5029c94124746
DutchJavaDev <Borismulder@outlook.com>
2025-06-14 14:17:44 +0000

    easy start stop

---
+++ MyMusicBoxApi/stopDev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Find and kill all the process
+# Kinda overkill....
+pkill -f "/tmp/go-build2246494624/b001/exe/main -port=8081 -devurl -sourceFolder=music_dev -outputExtension=opus"
+pkill -f "go run main.go -port=8081 -devurl -sourceFolder=music_dev -outputExtension=opus"
+pkill -f "/home/admin/.cache/go-build/cd/"
