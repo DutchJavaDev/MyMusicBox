620ead05042cd4fd80754ccd035153c907137850
DutchJavaDev <Borismulder@outlook.com>
2025-07-10 20:13:24 +0000

    propper release

--- MyMusicBoxApi/buildProd.sh
+++ MyMusicBoxApi/buildProd.sh
@@ -1,1 +1,6 @@
-go build -buildvcs=false -o ~/mymusicbox_production
+sudo systemctl stop mymusic
+sudo go build -buildvcs=false -o /home/admin/mymusicbox_production
+#sudo ls -l /home/admin/mymusicbox_production
+sudo systemctl start mymusic
+echo "Press CTRL + C to escape"
+sudo journalctl -u mymusic.service -f
