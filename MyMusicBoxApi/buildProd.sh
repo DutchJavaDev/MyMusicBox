56372111e785d3b9d263dfb790621f87b51f42f1
DutchJavaDev <Borismulder@outlook.com>
2025-07-12 22:15:35 +0000

    release script production

--- MyMusicBoxApi/buildProd.sh
+++
@@ -1,6 +0,0 @@
-sudo systemctl stop mymusic
-sudo go build -buildvcs=false -o /home/admin/mymusicbox_production
-#sudo ls -l /home/admin/mymusicbox_production
-sudo systemctl start mymusic
-echo "Press CTRL + C to escape"
-sudo journalctl -u mymusic.service -f
