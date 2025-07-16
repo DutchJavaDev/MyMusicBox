1d8262385c620e9216215647c6123dbb90b07a5d
DutchJavaDev <Borismulder@outlook.com>
2025-07-16 19:55:17 +0000

    description, unused method

--- MyMusicBoxApi/configuration/util.go
+++ MyMusicBoxApi/configuration/util.go
@@ -8,20 +8,12 @@ import (
 
 var Config models.Config
 
-func LoadConfig() {
-	flag.StringVar(&Config.DevPort, "port", "", "Development port else use default port")
-	flag.BoolVar(&Config.UseDevUrl, "devurl", false, "Have a dev prefix in the url")
-	flag.StringVar(&Config.SourceFolder, "sourceFolder", "music", "Output folder for data")
-	flag.StringVar(&Config.OutputExtension, "outputExtension", "opus", "Extension for ouput file")
+func LoadConfiguration() {
+	flag.StringVar(&Config.DevPort, "port", "", "-port=8081")
+	flag.BoolVar(&Config.UseDevUrl, "devurl", false, "-devurl")
+	flag.StringVar(&Config.SourceFolder, "sourceFolder", "music", "-sourceFolder=/path to source folder/")
+	flag.StringVar(&Config.OutputExtension, "outputExtension", "opus", "-outputExtension=opus,mp3,mp4 etc")
 	flag.Parse()
-}
-
-func GetApiGroupUrlV1() string {
-	if Config.UseDevUrl {
-		return "/dev/api/v1"
-	} else {
-		return "/api/v1"
-	}
 }
 
 func GetApiGroupUrl(version string) string {
